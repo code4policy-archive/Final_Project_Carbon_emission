d6ffa572a6daa58bf5512d769fdad0e7caad48c4
lmaharjan <58453420+lmaharjan@users.noreply.github.com>
2022-01-17 12:50:01 -0500

    Added a go to top button on take actions page

--- p6-take-action/drop.js
+++ p6-take-action/drop.js
@@ -129,7 +129,16 @@ drawbar("#vis-container");
 
 //Print Chart #2
 drawbar("#vis-container2");
+//--------------------------
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
 
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 
